0880ca1897680a48afd1e3361a7ba6c70caf0123
Nick Gerrard <gerrard.nick@gmail.com>
2022-02-05 13:16:42 -0800

    implemented main Q&A widget component

--- client/compile/Questions/questions.component.js
+++ client/compile/Questions/questions.component.js
@@ -1,16 +1,36 @@
 import React from 'react';
 import './questions.scss';
+import SearchBar from './subcomponents/SearchBar.jsx';
+import QuestionsList from './subcomponents/QuestionsList.jsx';
+import MoreQuestions from './subcomponents/MoreQuestions.jsx';
+import AddQuestion from './subcomponents/AddQuestion.jsx';
 
 
-var Questions = function (props){
+class Questions extends React.component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: '',
+      expanded: 0
+    };
+  }
 
-
-
-
-
-  return (<div className="questions"> Questions component</div>)
-
-
-
+  render() {
+    return (
+      <div className="questionsWidget">
+        <h6>QUESTIONS &amp; ANSWERS</h6>
+        <SearchBar />
+        <div className="questionsList">
+          <QuestionsList expanded={this.state.expanded}/>
+        </div>
+        <div className="questionButtons">
+          <MoreQuestions />
+          <div></div>
+          <AddQuestion />
+        </div>
+      </div>
+    )
+  }
 }
+
 export default Questions;
