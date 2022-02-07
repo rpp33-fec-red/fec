4c269ddfcf885b0b2810715d09e4b20373ef5cf7
Grant Mitchell <soundbreakr1@gmail.com>
2022-02-07 16:06:18 -0700

    made data api accessable to each component

--- client/compile/Overview/overview.component.js
+++ client/compile/Overview/overview.component.js
@@ -2,13 +2,33 @@ import React from 'react';
 import './overview.scss';
 
 
-var Overview = function (props){
+class Overview extends React.Component{
+
+  constructor(props){
+    super(props)
+    this.state = {
+      onProduct:{}
+    }
+    this.getData= this.getData.bind(this)
+  }
+
+  componentDidMount(){
+    this.getData();
+  }
+
+  getData(){
+    var that = this;
+    this.props.getProducts('products',{},function(data){
+      console.log(data)
+      that.setState({onProduct:data.results[0]})
+      console.log(that.state)
+    })
+  }
 
 
-
-
-
-  return (<div className="overview"> Overview component</div>)
+  render (){
+    return (<div className="overview"> Overview component</div>)
+  }
 
 
 
