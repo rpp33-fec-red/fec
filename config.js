cf2c0cb2d13cd441b90697c70456e302ab60d8f9
Grant Mitchell <soundbreakr1@gmail.com>
2022-02-07 16:06:18 -0700

    added data api

---
+++ config.js
@@ -0,0 +1,27 @@
+var campusCode ='hr-rpp'
+
+var options = {
+  APIURL:`https://app-hrsei-api.herokuapp.com/api/fec2/${campusCode}`,
+  database: null,
+  APIKEY:'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'
+}
+
+
+class config {
+constructor(deploy){
+  this.deploy;
+  this.options = options;
+  if (deploy) {
+
+  } else {
+
+  }
+  this.getOptions = this.getOptions.bind(this);
+}
+
+getOptions(){
+  return this.options;
+}
+
+}
+module.exports = config
