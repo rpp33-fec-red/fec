beb77d0c9352a136de1c5b7e47c944604a957db4
Alysha Gilliard <alyshargilliard@gmail.com>
2022-02-08 12:19:26 -0500

    Start organizing html elements in ReviewTitle

---
+++ client/compile/RatingsReviews/components/Ratings/RatingsSummary.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function RatingsSummary(props) {
+  return <p>RatingsSummary</p>;
+}
+
+export default RatingsSummary;
--- client/compile/RatingsReviews/components/Reviews/ReviewTile.js
+++ client/compile/RatingsReviews/components/Reviews/ReviewTile.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
 function ReviewTile () {
-  return <p>review tile</p>;
+  return (
+    <div className="review=tile">
+        <div className="star-rating">* * * * *</div>
+        <h3>Review Title...</h3>
+        <h4 className="date-of-review">Month DD, YYYY</h4>
+        <h4 className="review-summary">this is where the review summary would go</h4>
+        <p className="review-body">this is where the review body would go</p>
+        <div className="review-helpfulness-vote">
+          <p>Helpful? <a> Yes</a> (#)</p>
+        </div>
+    </div>
+  );
 }
 
 export default ReviewTile;
