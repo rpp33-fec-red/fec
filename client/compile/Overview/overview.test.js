daa9d7845f652f5c083a5c15ac22bc2e3639a546
Grant Mitchell <soundbreakr1@gmail.com>
2022-02-07 20:18:20 -0700

    overview change

--- client/compile/Overview/overview.test.js
+++ client/compile/Overview/overview.test.js
@@ -3,14 +3,12 @@ import React from 'react';
 // const TestRenderer = require('react-test-renderer'); // ES5 with npm
 import testData from './testdata.js'
 import Overview from './overview.component.js';
-console.log(Overview)
 import Model from '../model.js';
 var model = new Model(false);
 
 
 describe('component should render',function(){
   var fakeData = testData;
-
 
   var  getData = function (){
     return model.getData;
