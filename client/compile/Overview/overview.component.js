4fb1576aacb32bf071900a9358c0782981eefc3c
Grant Mitchell <soundbreakr1@gmail.com>
2022-02-09 15:23:26 -0700

    rebase

--- client/compile/Overview/overview.component.js
+++ client/compile/Overview/overview.component.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import './overview.scss';
+// import './overview.scss';
 
+import LeftContainer from './leftContainer/leftcontainer.component.js'
+import RightContainer from './rightContainer/rightcontainer.component.js'
 
+<<<<<<< HEAD
 var Overview = function (props){
 
 
@@ -9,6 +12,50 @@ var Overview = function (props){
 
 
   return (<div className="overview"> Overview component</div>)
+=======
+class Overview extends React.Component{
+
+  constructor(props){
+    super(props)
+    this.state = {
+      onProduct: {
+        id: 64620,
+        campus: 'hr-rpp',
+        name: 'Camo Onesie',
+        slogan: 'Blend in to your crowd',
+        description: 'The So Fatigues will wake you up and fit you in. This high energy camo will have you blending in to even the wildest surroundings.',
+        category: 'Jackets',
+        default_price: '140.00',
+        created_at: '2022-01-28T00:20:03.466Z',
+        updated_at: '2022-01-28T00:20:03.466Z'
+      }
+    }
+    this.getData= this.getData.bind(this)
+  }
+
+  componentDidMount(){
+    this.getData();
+  }
+
+  getData(){
+    var that = this;
+    this.props.getProducts('products',{},function(data){
+      console.log(data)
+      that.setState({onProduct:data.results[0]})
+      console.log(that.state)
+    })
+  }
+
+
+  render (){
+    return (<div className="overview">
+    <LeftContainer product={this.state.onProduct}/>
+    <RightContainer/>
+
+
+    </div>)
+  }
+>>>>>>> added test file and react test renderer
 
 
 
--- client/compile/Overview/overview.test.js
+++ client/compile/Overview/overview.test.js
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 //grab your functions to test  and import them to this file
 
 //https://jestjs.io/docs/getting-started
@@ -27,10 +28,37 @@ describe('component should render',function(){
     console.log('treeee',tree)
 
     expect(tree).toMatchSnapshot()
+=======
+import TestRenderer from 'react-test-renderer';
+import React from 'react';
+// const TestRenderer = require('react-test-renderer'); // ES5 with npm
+import testData from './testdata.js'
+import Overview from './overview.component.js';
+console.log(Overview)
+import Model from '../model.js';
+var model = new Model(false);
+
+
+describe('component should render',function(){
+var fakeData = testData;
+
+
+
+
+  test('should have overview component in browser',function(){
+
+    const tree = TestRenderer.create(<Overview getProducts={model.getData}/>)
+    var reactObj = tree.toJSON()
+    expect(reactObj.type).toMatchSnapshot()
+>>>>>>> added test file and react test renderer
   })
 
 
 
 
+<<<<<<< HEAD
 })
 >>>>>>> 35bf351... testing done for finding one component and added in package json update jest artifacts to restore snapshots
+=======
+})
+>>>>>>> added test file and react test renderer
