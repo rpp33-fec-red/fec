9ab2b98cdcf9a3935ed34fffbbe528412680d4ba
Grant Mitchell <35341268+grant350@users.noreply.github.com>
2022-02-09 17:47:47 -0700

    added data api (#15)

--- client/compile/app.js
+++ client/compile/app.js
@@ -7,28 +7,31 @@ import ReactDOM from 'react-dom';
  import Ratings from './Ratings/ratings.component.js'
  import Questions from './Questions/questions.component.js'
  import RelatedItems from './RelatedItems/relatedItems.component.js'
-
+import Model from './model.js';
+var model = new Model(false);
+console.log(model)
  //core css
+
  import './style.scss'
 class Main extends React.Component {
 
   constructor(props) {
     super(props)
     this.state = {
-      //api data
+
     }
   }
 
-  getData() {
+  componentDidMount(){
 
-    //api request
-    // update state
   }
+
+
 
   render(){
     return (<div className="main">
 
-      <Overview/>
+      <Overview getProducts={model.getData}/>
 
       <RelatedItems/>
       <Questions/>
