88d17ff5dac27adad101f3dcc63452e1f4439957
Alysha Gilliard <alyshargilliard@gmail.com>
2022-02-09 23:19:38 -0500

    Added test that checks rendering for Ratings component

--- client/compile/RatingsWidget/ratings.test.js
+++ client/compile/RatingsWidget/ratings.test.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import RatingsWidget from './components/RatingsWidget.js';
+import Reviews from './components/Reviews/Reviews.js'
 
-it('Ratings widget renders correctly', () => {
+it('RatingsWidget component renders correctly', () => {
   const tree = renderer
   .create(<RatingsWidget/>)
+  .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('Ratings component renders correctly', () => {
+  const tree = renderer
+  .create(<Ratings/>)
   .toJSON();
   expect(tree).toMatchSnapshot();
 });
