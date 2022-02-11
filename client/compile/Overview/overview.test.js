2676dd497c58d17ee145da6f5b7ee97f04950282
Grant Mitchell <35341268+grant350@users.noreply.github.com>
2022-02-10 19:28:13 -0700

    made changes updated master with a pull and put my model file back (#19)
    * added express
    
    * made data api accessable to each component
    
    * added readme
    
    * added test file and react test renderer
    
    * testing done for finding one component and added in package json update jest artifacts to restore snapshots
    
    * added webpack config fix for jsx
    
    * overview change
    
    * added files
    
    * resolved conflicts
    
    * render MAIN class not function

--- client/compile/Overview/overview.test.js
+++ client/compile/Overview/overview.test.js
@@ -1,3 +1,28 @@
-//grab your functions to test  and import them to this file
+import renderer from 'react-test-renderer';
+import React from 'react';
+// const TestRenderer = require('react-test-renderer'); // ES5 with npm
+import testData from './testdata.js'
+import Overview from './overview.component.js';
+import Model from '../model.js';
+var model = new Model(false);
 
-//https://jestjs.io/docs/getting-started
+
+describe('component should render',function(){
+  var fakeData = testData;
+
+  var  getData = function (){
+    return model.getData;
+  }
+
+  test('should have overview component in browser',function(){
+
+    var tree = renderer.create(<Overview getProducts={getData}/>).toJSON()
+    console.log('treeee',tree)
+
+    expect(tree).toMatchSnapshot()
+  })
+
+
+
+
+})
--- server/server.js
+++ server/server.js
@@ -1,53 +1,50 @@
 var express = require('express');
 var app = express();
 var port = 8080;
-var path = require('path');
-
+var path = require('path')
 app.use(express.static(path.join(__dirname,'client/public')));
+var config = require('../config');
+var options = new config(false);
+options = options.getOptions();
+var axios = require('axios')
+var bp = require('body-parser')
+app.use(bp.json())
+var cors = require('cors');
+
+
+app.use(cors());
+
+
+app.get('/getData',function(request, response) {
+  var url = options.APIURL;
+  if (request.query.route){
+    url+=`/${request.query.route}?`;
+  }
+  Object.keys(request.query).forEach((param)=>{
+    console.log('url',url)
+    console.log(param)
+    var value = request.query[param];
+    if (param !== 'route'){
+      url+=`&${param}=${value}`;
+    }
+  });
+  axios({
+    method: 'get',
+    url:url,
+    headers:{'authorization':`${options.APIKEY}`,'Accept':'*'}
+  }).then(function(results){
+    console.log(results);
+    if (results.data){
+      response.json({results:results.data});
+    } else {
+      response.json({Error: new Error('no data')})
+    }
+  }).catch(err=>{
+    response.json({results:[],Error:err});
+  });
+
+})
 
 app.listen(port,function(){
   console.log('listenening on ',port)
 })
-
-// var config = require('../config');
-// var options = new config(false);
-// options = options.getOptions();
-// var axios = require('axios')
-// var bp = require('body-parser')
-// app.use(bp.json())
-// var cors = require('cors');
-
-
-// app.use(cors());
-
-
-// app.get('/getData',function(request, response) {
-//   var url = options.APIURL;
-//   if (request.query.route){
-//     url+=`/${request.query.route}?`;
-//   }
-//   Object.keys(request.query).forEach((param)=>{
-//     console.log('url',url)
-//     console.log(param)
-//     var value = request.query[param];
-//     if (param !== 'route'){
-//       url+=`&${param}=${value}`;
-//     }
-//   });
-//   axios({
-//     method: 'get',
-//     url:url,
-//     headers:{'authorization':`${options.APIKEY}`,'Accept':'*'}
-//   }).then(function(results){
-//     console.log(results);
-//     if (results.data){
-//       response.json({results:results.data});
-//     } else {
-//       response.json({Error: new Error('no data')})
-//     }
-//   }).catch(err=>{
-//     response.json({results:[],Error:err});
-//   });
-
-// })
-
