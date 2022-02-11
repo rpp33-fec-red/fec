3b2cf98665ddc083952e34ca399676ba405657f3
Nick Gerrard <gerrard.nick@gmail.com>
2022-02-10 20:59:39 -0800

    rendered SearchBar with test

--- client/compile/Questions/Questions.jsx
+++ client/compile/Questions/Questions.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-//import SearchBar from './subcomponents/SearchBar.jsx';
+import SearchBar from './subcomponents/SearchBar.jsx';
 //import QuestionsList from './subcomponents/QuestionsList.jsx';
 //import MoreQuestions from './subcomponents/MoreQuestions.jsx';
 //import AddQuestion from './subcomponents/AddQuestion.jsx';
@@ -12,13 +12,30 @@ class QuestionsWidget extends React.Component {
       query: '',
       maxQuestionsDisplayed: 2
     };
+
+    this.handleSearch = this.handleSearch.bind(this);
+  }
+
+  handleSearch(event) {
+    event.preventDefault();
+    let query = event.target.value;
+
+    if (query.length > 2) {
+      this.setState({
+        query: query
+      });
+    } else if (this.state.query !== '') {
+      this.setState({
+        query: ''
+      });
+    }
   }
 
   render() {
     return (
       <div className="questionsWidget">
-        <h6>QUESTIONS AND ANSWERS</h6>
-        <p>REPLACE WITH SEARCHBAR COMPONENT</p>
+        <h6>QUESTIONS &amp; ANSWERS</h6>
+        <SearchBar search={this.handleSearch}/>
         <p>REPLACE WITH QUESTIONSLIST COMPONENT</p>
 
         <div className="questionButtons">
