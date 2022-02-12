e915c21e3adea7fdd7c395ce17a763d9c82ea68f
Alysha Gilliard <alyshargilliard@gmail.com>
2022-02-12 15:47:15 -0500

    Updated comment regarding refactoring

--- client/compile/RatingsWidget/components/Ratings/RatingsProductBreakdown.js
+++ client/compile/RatingsWidget/components/Ratings/RatingsProductBreakdown.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+// Going to refactor this component to be more modular. Will include in next pull request.
 function RatingsProductBreakdown(props) {
   return (
     <div className="ratings-product-breakdown">
