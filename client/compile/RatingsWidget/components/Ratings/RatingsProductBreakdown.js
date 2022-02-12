e8127ed1630f5deebd7076a7874a35f1ed835533
Alysha Gilliard <alyshargilliard@gmail.com>
2022-02-12 15:38:27 -0500

    Updated html attribute and directort reference

--- client/compile/RatingsWidget/components/Ratings/RatingsProductBreakdown.js
+++ client/compile/RatingsWidget/components/Ratings/RatingsProductBreakdown.js
@@ -3,7 +3,7 @@ import React from 'react';
 function RatingsProductBreakdown(props) {
   return (
     <div className="ratings-product-breakdown">
-      Ratings Product Breakdown
+      Ratings Product Breakdown Placeholder
     </div>
   );
 }
