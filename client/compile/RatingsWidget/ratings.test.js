f2cd22cc0fd453651bff920d07b0d1eedefaab4c
Alysha Gilliard <alyshargilliard@gmail.com>
2022-02-12 15:45:57 -0500

    Refactored code for readability and added more tests

--- client/compile/RatingsWidget/ratings.test.js
+++ client/compile/RatingsWidget/ratings.test.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import RatingsWidget from './components/RatingsWidget.js';
+import Ratings from './components/Ratings/Ratings.js';
+import Reviews from './components/Reviews/Reviews.js';
 
-describe('Ratings widget renders correctly', () => {
-  const tree = renderer
-  .create(<RatingsWidget/>)
-  .toJSON();
-  expect(tree).toMatchSnapshot();
+describe('Components render correctly', () => {
+
+  test('main ratings and reviews widget renders correctly', () => {
+    const tree = renderer
+    .create(<RatingsWidget/>)
+    .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('main reviews widget renders correctly', () => {
+    const tree = renderer
+    .create(<Reviews/>)
+    .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('main ratings widget renders correctly', () => {
+    const tree = renderer
+    .create(<Ratings/>)
+    .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
 });
