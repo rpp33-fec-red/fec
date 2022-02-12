c2b411ffeca1e5b36ec94a0f3fcb0b4c50f71b77
Alysha Gilliard <alyshargilliard@gmail.com>
2022-02-12 14:41:30 -0500

    Intitial html element for ratings product breakdown

--- client/compile/RatingsWidget/components/Ratings/RatingsProductBreakdown.js
+++ client/compile/RatingsWidget/components/Ratings/RatingsProductBreakdown.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function RatingsProductBreakdown(props) {
-  return <p>RatingsProductBreakdown</p>;
+  return (
+    <div className="ratings-product-breakdown">
+      Ratings Product Breakdown
+    </div>
+  );
 }
 
 export default RatingsProductBreakdown;
