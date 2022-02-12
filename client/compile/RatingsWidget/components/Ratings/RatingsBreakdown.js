7c696838ba8bd4658d79f54209e8f4718c9f5629
Alysha Gilliard <alyshargilliard@gmail.com>
2022-02-12 14:38:39 -0500

    Add intial html elements for RatingsBreakdown component

--- client/compile/RatingsWidget/components/Ratings/RatingsBreakdown.js
+++ client/compile/RatingsWidget/components/Ratings/RatingsBreakdown.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
 function RatingsBreakdown(props) {
-  return <p>ratingsBreakdown</p>;
+  return (
+    <div className="ratings-breakdown">
+      <div>5 stars ------------</div>
+      <div>4 stars ------------</div>
+      <div>3 stars ------------</div>
+      <div>2 stars ------------</div>
+      <div>1 stars ------------</div>
+    </div>
+  );
 }
 
 export default RatingsBreakdown;
