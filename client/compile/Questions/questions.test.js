eb25ae99e716b7625b08ba647803429fe72f972b
Nick Gerrard <gerrard.nick@gmail.com>
2022-02-13 11:22:18 -0800

    debugged implemented tests by passing in static data

--- client/compile/Questions/questions.test.js
+++ client/compile/Questions/questions.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import TestRenderer from 'react-test-renderer';
 import Questions from './Questions.jsx';
+import SearchBar from './subcomponents/SearchBar.jsx';
 import Question from './subcomponents/Question.jsx';
-import searchBarTree from './subcomponents/SearchBar.jsx';
 import QuestionsList from './subcomponents/QuestionsList.jsx';
+import { sampleData } from './sampleData.js';
 
 //https://jestjs.io/docs/getting-started
 
@@ -16,7 +17,7 @@ describe('Questions widget component', () => {
 });
 
 describe('SearchBar component', () => {
-  test('loads and displays searcbBar component', () => {
+  test('loads and displays SearcbBar component', () => {
     var searchBarTree = TestRenderer.create(<SearchBar />).toJSON();
 
     expect(searchBarTree).toMatchSnapshot();
@@ -25,16 +26,16 @@ describe('SearchBar component', () => {
 
 describe('QuestionsList component', () => {
   test('loads and displays QuestionsList component', () => {
-    var questionsListTree = TestRenderer.create(<QuestionsList />).toJSON();
+    var questionsListTree = TestRenderer.create(<QuestionsList questions={sampleData}/>).toJSON();
 
     expect(questionsListTree).toMatchSnapshot();
   });
 });
 
-// describe('Question component', () => {
-//   test('loads and displays Question component', () => {
-//     var questionTree = TestRenderer.create(<Question />).toJSON();
+describe('Question component', () => {
+  test('loads and displays Question component', () => {
+    var questionTree = TestRenderer.create(<Question question={sampleData.results[0]} />).toJSON();
 
-//     expect(questionTree).toMatchSnapshot();
-//   });
-// });
+    expect(questionTree).toMatchSnapshot();
+  });
+});
