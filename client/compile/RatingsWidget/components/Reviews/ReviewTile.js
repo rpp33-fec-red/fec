e457cc18983bb1e51969797e945d3130a210efc2
Alysha Gilliard <alyshargilliard@gmail.com>
2022-02-13 16:30:57 -0500

    Reformatted date

--- client/compile/RatingsWidget/components/Reviews/ReviewTile.js
+++ client/compile/RatingsWidget/components/Reviews/ReviewTile.js
@@ -1,18 +1,38 @@
 import React from 'react';
 
-function ReviewTile (props) {
-  return (
-    <div className="review=tile">
+class ReviewTile extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      recommended: false
+    }
+    this.convertDate = this.convertDate.bind(this);
+  }
+
+  convertDate (date) {
+    let dateObject = new Date (date);
+    var options = { month: 'long'};
+    const month = new Intl.DateTimeFormat('en-US', options).format(dateObject);
+    const day = dateObject.getDate();
+    const year = dateObject.getFullYear();
+    return month + ' ' + day + ', ' + year;
+  }
+
+  render () {
+    let formattedDate = this.convertDate(this.props.review.date);
+    return (
+      <div className="review=tile">
         <p className="star-rating">* * * * *</p>
-        <h4 className="review-summary">{props.review.summary}</h4>
-        <h4 className="date-of-review">{props.review.date}</h4>
-        <h4 className="reviewer-name">{props.review.reviewer_name}</h4>
-        <p className="review-body">{props.review.body}</p>
+        <h4 className="review-summary">{this.props.review.summary}</h4>
+        <h4 className="date-of-review">{formattedDate}</h4>
+        <h4 className="reviewer-name">{this.props.review.reviewer_name}</h4>
+        <p className="review-body">{this.props.review.body}</p>
         <div className="review-helpfulness-voting">
-          <p>Helpful? <a> Yes</a> ({props.review.helpfulness})</p>
+          <p>Helpful? <a> Yes</a> ({this.props.review.helpfulness})</p>
         </div>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default ReviewTile;
