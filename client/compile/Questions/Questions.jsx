26bc9994e4cd4ab1d398eb9200fc7b97851bf54e
Nick Gerrard <gerrard.nick@gmail.com>
2022-02-14 21:55:59 -0800

    addressed merge conflicts

--- client/compile/Questions/Questions.jsx
+++ client/compile/Questions/Questions.jsx
@@ -12,13 +12,9 @@ class QuestionsWidget extends React.Component {
     super(props);
     this.state = {
       query: '',
-<<<<<<< HEAD
       maxQuestionsDisplayed: 2,
       allQuestionsDisplayed: false,
-      questionData: sampleData.results
-=======
       questionsData: sampleData.results
->>>>>>> c320f6a90dc5e09a0d0358d7c4b4ebc7ac7b037d
     };
 
     this.handleSearch = this.handleSearch.bind(this);
@@ -44,19 +40,8 @@ class QuestionsWidget extends React.Component {
       <div className="questionsWidget">
         <h6>QUESTIONS &amp; ANSWERS</h6>
         <SearchBar search={this.handleSearch}/>
-<<<<<<< HEAD
         <QuestionsList questions={this.state.questionsData}/>
-=======
-        <QuestionsList questions={this.state.questionData}/>
-<<<<<<< HEAD
         <QuestionButtons allQuestionsDisplayed={this.state.allQuestionsDisplayed} />
-=======
->>>>>>> 24a7ce624f601bebc077e3e573cd691fa465ca69
-        <div className="questionButtons">
-          <p>REPLACE WITH MOREQUESTIONS COMPONENT</p>
-          <p>REPLACE WITH ADDQUESTIONS COMPONENT</p>
-        </div>
->>>>>>> c320f6a90dc5e09a0d0358d7c4b4ebc7ac7b037d
       </div>
     );
   }
