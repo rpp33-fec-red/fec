b5d30da729eda5a69640475cf08308800433fa62
Alysha Gilliard <alyshargilliard@gmail.com>
2022-02-15 12:31:27 -0500

    Add describe block to ReviewTile component

--- client/compile/RatingsWidget/ratings.test.js
+++ client/compile/RatingsWidget/ratings.test.js
@@ -3,6 +3,7 @@ import renderer from 'react-test-renderer';
 import RatingsWidget from './components/RatingsWidget.js';
 import Ratings from './components/Ratings/Ratings.js';
 import Reviews from './components/Reviews/Reviews.js';
+import ReviewTile from './components/Reviews/ReviewTile.js';
 
 describe('Components render correctly', () => {
 
@@ -28,3 +29,25 @@ describe('Components render correctly', () => {
   });
 
 });
+
+describe('ReviewTile component', () => {
+
+  test('renders correctly', () => {
+    const tree = renderer
+    .create(<ReviewTile/>)
+    .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  // test for convert date function
+
+  // check that review summary is capped at 60 characters
+
+  // test for show more button
+  // test to make sure only 250 characters show by default
+
+  // test to check if "I recommend this product shows if product is recommended"
+
+  // test for rating helpfulness
+});
+
