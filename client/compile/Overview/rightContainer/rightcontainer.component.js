e1451484fe236e96217f9db44e2e8fb02d927d92
Grant Mitchell <soundbreakr1@gmail.com>
2022-02-20 03:50:00 -0700

    overflow hidden for page to stay together

--- client/compile/Overview/rightContainer/rightcontainer.component.js
+++ client/compile/Overview/rightContainer/rightcontainer.component.js
@@ -7,6 +7,7 @@ class RightContainer extends React.Component{
 
   constructor(props){
     super(props);
+    console.log(props.onProduct);
   }
 
   //right container display flex;
@@ -20,6 +21,9 @@ class RightContainer extends React.Component{
 
       <div className="main-content">
         <div className="reviewWrapper"><StarsComponent className="star-ct" color={'red'} count={5}></StarsComponent><a onClick={this.clickedReviews}>readall reviews</a></div>
+        <span className="mediumText">catagory</span>
+        <div className="title"> {this.props.onProduct.name}</div>
+        <span className="price">$ {this.props.onProduct.default_price}</span>
       </div>
       <div className="style-selectors">
 
