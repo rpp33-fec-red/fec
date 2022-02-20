fb50c0fe90725556eec2a51f6d573bfad905d832
Alysha Gilliard <alyshargilliard@gmail.com>
2022-02-20 09:10:00 -0500

    Refactored into class component

--- client/compile/RatingsWidget/Reviews/ReviewsList.js
+++ client/compile/RatingsWidget/Reviews/ReviewsList.js
@@ -3,16 +3,21 @@ import ReviewTile from './ReviewTile.js';
 import ReviewsSorting from './ReviewsSorting.js';
 import PropTypes from 'prop-types';
 
-// The purpose of this component is to display and filter the reviews based on user input.
-function ReviewsList (props) {
-  return (
-    <div className ="reviews-list">
-      <ReviewsSorting/>
-      {props.reviews.map((review) => {
-        return <ReviewTile key={review.review_id} review={review}/>;
-      })}
-    </div>
-  );
+class ReviewsList extends React.Component {
+  constructor (props) {
+    super(props);
+
+  }
+  render () {
+    return (
+      <div className ="reviews-list">
+        <ReviewsSorting/>
+        {this.props.reviews.map((review) => {
+          return <ReviewTile key={review.review_id} review={review}/>;
+        })}
+      </div>
+    );
+  }
 }
 
 ReviewsList.propTypes = {
