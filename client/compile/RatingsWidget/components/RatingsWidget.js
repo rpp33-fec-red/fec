65fa5ee37499b8667885e3547716bf4496668a92
Grant Mitchell <soundbreakr1@gmail.com>
2022-02-20 02:40:40 -0700

    fixed ratingsWidget missing

---
+++ client/compile/RatingsWidget/components/RatingsWidget.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import '../ratings.scss';
+import Reviews from './Reviews/Reviews.js';
+import Ratings from './Ratings/Ratings.js';
+import reviewsData from '../sample_data.js';
+import PropTypes from 'prop-types';
+
+class RatingsWidget extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sortedBy: 'relevance',
+      filteredBy: {
+        oneStar: false,
+        twoStar: false,
+        threeStar: false,
+        fourStar: false,
+        fiveStar: false,
+      }
+    }
+    this.getData = this.getData.bind(this);
+  }
+
+
+  componentDidMount () {
+    this.getData();
+  }
+
+
+  getData () {
+    var options = {
+      sort: 'newest'
+    };
+    this.props.getReviews('GET', ['reviews', 64620, ''], options, (data) => {
+      console.log('these are the reviews:', data);
+    });
+  }
+
+
+  render () {
+    return (
+      <div className="ratings-and-reviews">
+        <Ratings/>
+        <Reviews reviews={reviewsData.results} />
+      </div>
+    );
+  }
+}
+
+RatingsWidget.propTypes = {
+  getReviews: PropTypes.any
+};
+
+
+export default RatingsWidget;
