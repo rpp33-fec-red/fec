e69d3af89f504d7549c633f8c31c1cd58571bf46
Alysha Gilliard <alyshargilliard@gmail.com>
2022-02-24 10:27:35 -0500

    Update sample data for testing

--- client/compile/RatingsWidget/sample_data.js
+++ client/compile/RatingsWidget/sample_data.js
@@ -60,6 +60,56 @@ var reviewsData = {
       }
 
       ]
+    },
+    {
+      "review_id": 5,
+      "rating": 3,
+      "summary": "I'm enjoying wearing these shades because they block the sun from my eyes and they are stylish.",
+      "recommend": true,
+      "response": null,
+      "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eu elementum ligula, in feugiat purus. Aenean sed nisl imperdiet, ultrices neque sit amet, vehicula lorem. Duis ut gravida dolor, ut ultricies nunc. Pellentesque ultrices efficitur elementum. Vestibulum et lacinia diam, ac hendrerit lacus. Nam a justo vitae libero dictum rhoncus id eu lectus. Donec aliquam elit nec massa vulputate, sit amet bibendum nunc varius. Aliquam sed tristique lectus, vitae rutrum lacus. Quisque ut nibh interdum felis porttitor ultricies eget auctor nunc. Vivamus vestibulum laoreet justo. Morbi iaculis nibh eu risus fermentum, id scelerisque ex hendrerit. Aliquam dui ex, blandit vitae suscipit vitae, lobortis vel enim. Donec lorem nisi, luctus vitae enim non, volutpat bibendum leo. Sed id augue eget tortor accumsan imperdiet. Duis at faucibus nulla. Proin volutpat massa ut velit suscipit, et faucibus tortor consequat. Vivamus maximus quam quis neque mattis iaculis. Vestibulum sed condimentum nibh, rutrum rhoncus nisl. Nulla condimentum pellentesque sem sed maximus. Donec cursus purus arcu, ac malesuada velit facilisis pulvinar. Donec non massa vestibulum, suscipit enim quis, vestibulum nisl. In consequat ex sed vulputate accumsan. Integer nec velit vitae nulla tempor aliquam. In vulputate mi quis ligula pharetra, eu pharetra tellus venenatis. Quisque gravida nibh a lorem commodo ornare a vitae lacus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Ut molestie ante urna, nec bibendum augue varius sit amet. Sed vitae consectetur tellus, vel congue ex. Sed bibendum pellentesque scelerisque. Nulla vitae sodales sem. Nulla volutpat nulla et enim dapibus sollicitudin. Proin consectetur sodales dapibus. Nullam sagittis sem nisi, at condimentum metus interdum id.",
+      "date": "2020-07-24T05:00:00.000Z",
+      "reviewer_name": "anotherUser",
+      "helpfulness": 8,
+      "photos": [{
+        "id": 1,
+        "url": "https://images.unsplash.com/photo-1560570803-7474c0f9af99?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=975&q=80"
+      },
+      {
+        "id": 2,
+        "url": "https://images.unsplash.com/photo-1560570803-7474c0f9af99?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=975&q=80"
+      },
+      {
+        "id": 3,
+        "url": "https://images.unsplash.com/photo-1560570803-7474c0f9af99?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=975&q=80"
+      }
+
+      ]
+    },
+    {
+      "review_id": 6,
+      "rating": 3,
+      "summary": "I'm enjoying wearing these shades because they block the sun from my eyes and they are stylish.",
+      "recommend": true,
+      "response": null,
+      "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eu elementum ligula, in feugiat purus. Aenean sed nisl imperdiet, ultrices neque sit amet, vehicula lorem. Duis ut gravida dolor, ut ultricies nunc. Pellentesque ultrices efficitur elementum. Vestibulum et lacinia diam, ac hendrerit lacus. Nam a justo vitae libero dictum rhoncus id eu lectus. Donec aliquam elit nec massa vulputate, sit amet bibendum nunc varius. Aliquam sed tristique lectus, vitae rutrum lacus. Quisque ut nibh interdum felis porttitor ultricies eget auctor nunc. Vivamus vestibulum laoreet justo. Morbi iaculis nibh eu risus fermentum, id scelerisque ex hendrerit. Aliquam dui ex, blandit vitae suscipit vitae, lobortis vel enim. Donec lorem nisi, luctus vitae enim non, volutpat bibendum leo. Sed id augue eget tortor accumsan imperdiet. Duis at faucibus nulla. Proin volutpat massa ut velit suscipit, et faucibus tortor consequat. Vivamus maximus quam quis neque mattis iaculis. Vestibulum sed condimentum nibh, rutrum rhoncus nisl. Nulla condimentum pellentesque sem sed maximus. Donec cursus purus arcu, ac malesuada velit facilisis pulvinar. Donec non massa vestibulum, suscipit enim quis, vestibulum nisl. In consequat ex sed vulputate accumsan. Integer nec velit vitae nulla tempor aliquam. In vulputate mi quis ligula pharetra, eu pharetra tellus venenatis. Quisque gravida nibh a lorem commodo ornare a vitae lacus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Ut molestie ante urna, nec bibendum augue varius sit amet. Sed vitae consectetur tellus, vel congue ex. Sed bibendum pellentesque scelerisque. Nulla vitae sodales sem. Nulla volutpat nulla et enim dapibus sollicitudin. Proin consectetur sodales dapibus. Nullam sagittis sem nisi, at condimentum metus interdum id.",
+      "date": "2020-07-24T05:00:00.000Z",
+      "reviewer_name": "different person",
+      "helpfulness": 8,
+      "photos": [{
+        "id": 1,
+        "url": "https://images.unsplash.com/photo-1560570803-7474c0f9af99?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=975&q=80"
+      },
+      {
+        "id": 2,
+        "url": "https://images.unsplash.com/photo-1560570803-7474c0f9af99?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=975&q=80"
+      },
+      {
+        "id": 3,
+        "url": "https://images.unsplash.com/photo-1560570803-7474c0f9af99?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=975&q=80"
+      }
+
+      ]
     }
   ]
 };
