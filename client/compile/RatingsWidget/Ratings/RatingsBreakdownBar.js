6108dc735b943f2587d0e2e2e10f9da792fabafb
Alysha Gilliard <alyshargilliard@gmail.com>
2022-02-28 22:50:14 -0500

    Add error handling for bar width

--- client/compile/RatingsWidget/Ratings/RatingsBreakdownBar.js
+++ client/compile/RatingsWidget/Ratings/RatingsBreakdownBar.js
@@ -7,7 +7,7 @@ function RatingsBreakdownBar (props) {
       <div className="star-label">{props.value + ' stars'}</div>
       <svg viewBox="0 0 100 5" xmlns="http://www.w3.org/2000/svg" style={{"fill": "grey"}}>
         <rect width="100%" height="5"/>
-        <rect width={props.ratingPercentage + '%'} height="5" style={{"fill": "green", "zIndex": "1"}}/>
+        <rect width={props.ratingPercentage || 0 + '%'} height="5" style={{"fill": "green", "zIndex": "1"}}/>
       </svg>
       <div className="review-number-label">{props.reviewNumber}</div>
     </div>
