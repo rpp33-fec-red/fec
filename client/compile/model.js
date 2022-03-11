8c7369f493024f02ac361638d5eb249c168ffa9b
Alysha Gilliard <alyshargilliard@gmail.com>
2022-03-10 23:21:00 -0500

    Add photos ratings + update model url (#77)
    * Refactoring to use form data
    
    * Update helper functions to use form data
    
    * Working on uploading to s3
    
    * Able to send photo from form to server
    
    * Working on s3
    
    * Reviews submitted
    
    * Removed console and disabled ESLint
    
    * Adjusted server formatting
    
    * Update formatting
    
    * Updated variable declaration
    
    * Update server host
    
    * Update comment for image validatation
    
    * Updated model file

--- client/compile/model.js
+++ client/compile/model.js
@@ -1,16 +1,8 @@
-var config = {
-  'serverURL':'/',
-  "testURL":'http://localhost:8080/'
-};
 import $ from 'jquery';
 class Model {
 
-  constructor(deploy) {
-    this.deploy = deploy;
-    this.url = config.serverURL;
-    if (!deploy) {
-      this.url = config.testURL;
-    }
+  constructor() {
+    this.url = '/';
     this.getData = this.getData.bind(this);
   }
 
