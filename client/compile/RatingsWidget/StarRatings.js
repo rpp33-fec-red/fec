39028cdf82b0717a1e777df1810fb0ba450af18c
Alysha Gilliard <alyshargilliard@gmail.com>
2022-03-12 00:35:23 -0500

    Styling Ratings  (#79)
    * Refactoring to use form data
    
    * Update helper functions to use form data
    
    * Working on uploading to s3
    
    * Able to send photo from form to server
    
    * Working on s3
    
    * Reviews submitted
    
    * Removed console and disabled ESLint
    
    * Adjusted server formatting
    
    * Update formatting
    
    * Updated variable declaration
    
    * Update server host
    
    * Update comment for image validatation
    
    * Add margin for widget
    
    * Updated styling for overall page font and color
    
    * Remove bold from reviews and ratings
    
    * Error handling for no reviews
    
    * Working on styling
    
    * Updated breakdown bar
    
    * Updated styling for stars
    
    * Update review summary styling
    
    * Adjusted product breakdown bar
    
    * Update sorting styling
    
    * Updated formatting for reviews
    
    * Update count for review breakdown
    
    * Update sorting review count
    
    * Update formatting for buttons
    
    * Update search placeholder text
    
    * Update recommend margin
    
    * Updated styling
    
    * Updated ratings count for breakdown
    
    * Working on readme
    
    * Updated styling for adding a new review
    
    * Updated README
    
    * Updated README
    
    * Update README
    
    * Removed README

--- client/compile/RatingsWidget/StarRatings.js
+++ client/compile/RatingsWidget/StarRatings.js
@@ -13,9 +13,14 @@ function createStarComponent (starRating, clickHandler) {
         array.push(<svg key={i}
           className="star"
           xmlns="http://www.w3.org/2000/svg"
-          enableBackground="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill='#f8d448' ><g>
-            <rect fill="none" height="24" width="24" x="0"/>
-            <polygon onClick={(event) => { clickHandler ? clickHandler(parseInt(event.target.id)) : null; }} id={i + 1} points="14.43,10 12,2 9.57,10 2,10 8.18,14.41 5.83,22 12,17.31 18.18,22 15.83,14.41 22,10" stroke='#646464'/></g></svg> );
+          enableBackground="new 0 0 15 15" height="15" viewBox="0 0 15 15" width="15" fill='#f8d448' ><g>
+            <rect fill="none" height="15" width="15" x="0"/>
+            <polygon
+              onClick={(event) => { clickHandler ? clickHandler(parseInt(event.target.id)) : null; }}
+              id={i + 1}
+              points="14.43,10 12,2 9.57,10 2,10 8.18,14.41 5.83,22 12,17.31 18.18,22 15.83,14.41 22,10"
+              stroke='#646464'
+              transform="scale(.60)"/></g></svg> );
       }
 
       let description = {
@@ -30,7 +35,7 @@ function createStarComponent (starRating, clickHandler) {
 
       return (
         <div className="star-rating-input">
-          <div className="starContainer" style={{"display": "inline-flex", "alignItems": "center", "position": "relative", "paddingLeft": "0.5rem"}}>
+          <div className="starContainer" style={{"display": "inline-flex", "alignItems": "center", "position": "relative"}}>
             <div className="starBase" style={{ "display": "flex", "width": "100%"}}>{array}</div>
             <div className="starOverlay" style={{ "width": `${100-(starRating * 20)}%`, "backgroundColor": "white", "mixBlendMode": "color", "opacity": "unset", "position": "absolute", "top": "0", "right": "0", "bottom": "0", "zIndex": "1", "pointerEvents": "none" }}></div>
           </div>
