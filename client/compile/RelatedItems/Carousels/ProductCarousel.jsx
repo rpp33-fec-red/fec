cae2ec0d1f318b7fbcd99c27bc7aaf4719efeb4e
thaotpnguyen <ntpt08@gmail.com>
2022-03-11 22:35:25 -0800

    update: chevron arrow only show when there is more cards to show

--- client/compile/RelatedItems/Carousels/ProductCarousel.jsx
+++ client/compile/RelatedItems/Carousels/ProductCarousel.jsx
@@ -41,7 +41,7 @@ const ProductCarousel = (props) => {
       <div className="productCarousel">
         {current === 0 
           ? (<></>)
-          : (numberOfSets > 0 && <button className="chevron-left" onClick={handleLeft}><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-chevron-left" viewBox="0 0 16 16"><path fillRule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/></svg></button>)
+          : (numberOfSets > 1 && <button className="chevron-left" onClick={handleLeft}><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-chevron-left" viewBox="0 0 16 16"><path fillRule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/></svg></button>)
         }
         {sets[current] && sets[current].map((id) => {
           return (
@@ -55,7 +55,7 @@ const ProductCarousel = (props) => {
           );
         })}
         {current === 0 
-          ? (numberOfSets > 0 && <button className="chevron-right" onClick={handleRight}><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-chevron-right" viewBox="0 0 16 16"><path fillRule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/></svg></button>)
+          ? (numberOfSets > 1 && <button className="chevron-right" onClick={handleRight}><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-chevron-right" viewBox="0 0 16 16"><path fillRule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/></svg></button>)
           : (<></>)
         }
       </div>
